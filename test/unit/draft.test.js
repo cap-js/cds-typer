fe85263c77d1baef587bcf61dce9476d0a1d2d0b
Daniel O'Grady <daniel.o-grady@sap.com>
2023-08-24 17:44:41 +0200

    Adjust test cases

--- test/unit/draft.test.js
+++ test/unit/draft.test.js
@@ -10,6 +10,8 @@ const dir = locations.testOutput('draft_test')
 
 describe('@odata.draft.enabled', () => {
     let ast
+    const draftable_ = entity => ast.find(n => n.name === entity && n.members.find(({name}) => name === 'drafts'))
+    const draftable = entity => draftable_(entity) && draftable_(`${entity}_`)
 
     beforeAll(async () => {
         await fs.unlink(dir).catch(err => {})
@@ -20,67 +22,48 @@ describe('@odata.draft.enabled', () => {
         ast = new ASTWrapper(path.join(paths[1], 'index.ts')).tree
     })
 
-    test('Direct Annotation', async () => {
-        expect(ast.find(n => n.name === 'A' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'A_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-    })
+    test('Direct Annotation', async () => expect(draftable('A')).toBeTruthy())
 
-    test('First Level Inheritance', async () => {
-        expect(ast.find(n => n.name === 'B' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'B_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-    })
+    test('First Level Inheritance', async () => expect(draftable('B')).toBeTruthy())
 
-    test('Explicit Override via Inheritance', async () => {
-        expect(ast.find(n => n.name === 'C' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'C_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-    })
+    test('Explicit Override via Inheritance', async () => expect(draftable('C')).not.toBeTruthy())
 
-    test('Inheritance of Explicit Override', async () => {
-        expect(ast.find(n => n.name === 'D' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'D_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-    })
+    test('Inheritance of Explicit Override', async () => expect(draftable('D')).not.toBeTruthy())
 
-    test('Declaration With true', async () => {
-        expect(ast.find(n => n.name === 'E' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'E_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-    })
+    test('Declaration With true', async () => expect(draftable('E')).toBeTruthy())
 
-    test('Multiple Inheritance With Most Significant true', async () => {
-        expect(ast.find(n => n.name === 'F' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'F_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-    })
+    test('Multiple Inheritance With Most Significant true', async () => expect(draftable('F')).toBeTruthy())
 
-    test('Multiple Inheritance With Most Significant false', async () => {
-        expect(ast.find(n => n.name === 'G' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'G_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-    })
+    test('Multiple Inheritance With Most Significant false', async () => expect(draftable('G')).not.toBeTruthy())
 
     test('Draftable by Association/ Composition', async () => {
-        expect(ast.find(n => n.name === 'H' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'H_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'I' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'I_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'J' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'J_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'K' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'K_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
+        expect(draftable('H')).not.toBeTruthy()
+        expect(draftable('I')).not.toBeTruthy()
+        expect(draftable('J')).not.toBeTruthy()
+        expect(draftable('K')).not.toBeTruthy()
     })
 
     test('Unchanged by Association/ Composition', async () => {
-        expect(ast.find(n => n.name === 'L' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'L_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'M' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'M_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'N' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'N_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'O' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
-        expect(ast.find(n => n.name === 'O_' && n.members.find(({name}) => name === 'drafts' ))).not.toBeTruthy()
+        expect(draftable('L')).not.toBeTruthy()
+        expect(draftable('M')).not.toBeTruthy()
     })
 
     test('Precedence Over Explicit Annotation', async () => {
-        expect(ast.find(n => n.name === 'P' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'P_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'Q' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
-        expect(ast.find(n => n.name === 'Q_' && n.members.find(({name}) => name === 'drafts' ))).toBeTruthy()
+        expect(draftable('P')).toBeTruthy()
+        expect(draftable('Q')).toBeTruthy()
+    })
+
+    test('Via Projection', async () => expect(draftable('PA')).toBeTruthy())
+
+    test('Transitive Via Projection', async () => {
+        expect(draftable('ProjectedReferrer')).toBeTruthy()
+        expect(draftable('Referrer')).toBeTruthy()
+        expect(draftable('Referenced')).toBeTruthy()
+    })
+
+    test('Transitive Via Multiple Levels of Projection', async () => {
+        expect(draftable('Foo')).toBeTruthy()
+        expect(draftable('ProjectedFoo')).toBeTruthy()
+        expect(draftable('ProjectedProjectedFoo')).toBeTruthy()
     })
 })
--- test/unit/files/draft/model.cds
+++ test/unit/files/draft/model.cds
@@ -11,19 +11,41 @@ entity E: D {}
 entity F: C,E {}
 entity G: E,C {}
 
-// should become draftable
+// don't become draftable
 entity H { ref: Association to A }
 entity I { ref: Association to many A }
+// should not become draftable
 entity J { ref: Composition of A }
 entity K { ref: Composition of many A }
 
 // should not
-entity L { ref: Association to C }
-entity M { ref: Association to many C }
-entity N { ref: Composition of C }
-entity O { ref: Composition of many C }
+entity L { ref: Composition of C }
+entity M { ref: Composition of many C }
+
+@odata.draft.enabled: true
+entity N { ref: Composition of many O }
+// ! should be enabled 
+entity O {}
 
 @odata.draft.enabled: true
 entity P { ref: Association to C }
 @odata.draft.enabled: false
-entity Q { ref: Association to A }
+entity Q { }
+@odata.draft.enabled: true
+entity R { ref: Composition of Q }
+
+entity PA as projection on A {}
+
+// propagate from projection to referenced entity
+entity Referenced {}
+entity Referrer {
+    ref: Association to Referenced
+}
+@odata.draft.enabled: true
+entity ProjectedReferrer as projection on Referrer {}
+
+// propagate over two levels of projection
+entity Foo {}
+entity ProjectedFoo as projection on Foo {}
+@odata.draft.enabled: true
+entity ProjectedProjectedFoo as projection on ProjectedFoo {}
