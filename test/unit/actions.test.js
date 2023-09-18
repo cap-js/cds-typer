4254c1de8f922b3eccc8c1f088b3ecb39da18528
Daniel O'Grady <daniel.o-grady@sap.com>
2023-09-18 13:26:36 +0200

    Generate bound operations into static .actions property

--- test/unit/actions.test.js
+++ test/unit/actions.test.js
@@ -13,12 +13,12 @@ describe('Actions', () => {
     test('Bound', async () => {
         const paths = await cds2ts('actions/model.cds', { outputDirectory: dir, inlineDeclarations: 'structured' })
         const astw = new ASTWrapper(path.join(paths[1], 'index.ts'))
-        checkFunction(astw.getAspectProperty('_EAspect', 'f'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        const actions = astw.getAspectProperty('_EAspect', 'actions')
+        expect(actions.modifiers.some(check.isStatic)).toBeTruthy()
+        checkFunction(actions.type.members.find(fn => fn.name === 'f'), {
             parameterCheck: ({members: [fst]}) => fst.name === 'x' && check.isString(fst.type)
         })
-        checkFunction(astw.getAspectProperty('_EAspect', 'g'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 'g'), {
             parameterCheck: ({members: [fst, snd]}) => {
                 const fstCorrect = fst.name === 'a' && fst.type.members[0].name === 'x' && check.isNumber(fst.type.members[0].type)
                     && fst.type.members[1].name === 'y' && check.isNumber(fst.type.members[1].type)
@@ -44,21 +44,20 @@ describe('Actions', () => {
     test('Bound Returning External Type', async () => {
         const paths = await cds2ts('actions/model.cds', { outputDirectory: dir, inlineDeclarations: 'structured' })
         const astw = new ASTWrapper(path.join(paths[1], 'index.ts'))
-        checkFunction(astw.getAspectProperty('_EAspect', 'f'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        const actions = astw.getAspectProperty('_EAspect', 'actions')
+        expect(actions.modifiers.some(check.isStatic)).toBeTruthy()
+        checkFunction(actions.type.members.find(fn => fn.name === 'f'), {
             callCheck: signature => check.isAny(signature),
             parameterCheck: ({members: [fst]}) => fst.name === 'x' && check.isString(fst.type),
             returnTypeCheck: returns => check.isAny(returns)
         })
 
-        checkFunction(astw.getAspectProperty('_EAspect', 'k'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 'k'), {
             callCheck: ({full}) => full === '_elsewhere.ExternalType',
             returnTypeCheck: ({full}) => full === '_elsewhere.ExternalType'
         })
 
-        checkFunction(astw.getAspectProperty('_EAspect', 'l'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 'l'), {
             callCheck: ({full}) => full === '_.ExternalInRoot',
             returnTypeCheck: ({full}) => full === '_.ExternalInRoot'
         })
@@ -84,21 +83,22 @@ describe('Actions', () => {
     test('Bound Expecting $self Arguments', async () => {
         const paths = await cds2ts('actions/model.cds', { outputDirectory: dir, inlineDeclarations: 'structured' })
         const astw = new ASTWrapper(path.join(paths[1], 'index.ts'))
+        const actions = astw.getAspectProperty('_EAspect', 'actions')
+        expect(actions.modifiers.some(check.isStatic)).toBeTruthy()
+        
+
         // mainly make sure $self parameter is not present at all
-        checkFunction(astw.getAspectProperty('_EAspect', 's1'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 's1'), {
             callCheck: signature => check.isAny(signature),
             returnTypeCheck: returns => check.isAny(returns),
             parameterCheck: ({members}) => members.length === 0
         })
-        checkFunction(astw.getAspectProperty('_EAspect', 'sn'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 'sn'), {
             callCheck: signature => check.isAny(signature),
             returnTypeCheck: returns => check.isAny(returns),
             parameterCheck: ({members}) => members.length === 0           
         })
-        checkFunction(astw.getAspectProperty('_EAspect', 'sx'), {
-            modifiersCheck: modifiers => modifiers.some(check.isStatic),
+        checkFunction(actions.type.members.find(fn => fn.name === 'sx'), {
             callCheck: signature => check.isAny(signature),
             returnTypeCheck: returns => check.isAny(returns),
             parameterCheck: ({members: [fst]}) => check.isNumber(fst.type)
