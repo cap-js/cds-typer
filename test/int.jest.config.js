4cb83e6de0fe0db8c5d12530c62d165ad84c053f
Daniel O'Grady <daniel.o-grady@sap.com>
2024-02-22 11:21:26 +0100

    Disable integration tests

--- test/int.jest.config.js
+++ test/int.jest.config.js
@@ -1,6 +1,8 @@
 // as xmake does not allow requests towards external resources (github.com),
 // we need to split tests referring to cap-cloud-samples into their own
 // test suite.
+// FIXME: reneable integration tests or merge them with unit tests
+/*
 module.exports = {
     displayName: "integration",
     rootDir: '..',
@@ -8,3 +10,4 @@ module.exports = {
     globalSetup: "<rootDir>/test/integration/setup.js",
     cache: false
 }
+*/
