664cd99ca820d4dc2601ca684705c75a3961b014
Daniel O'Grady <daniel.o-grady@sap.com>
2024-02-28 16:09:19 +0100

    Add test case

--- test/unit/type.test.js
+++ test/unit/type.test.js
@@ -1,23 +1,30 @@
 'use strict'
 
 const { locations, prepareUnitTest } = require('../util')
+const { check, checkFunction } = require('../ast')
 
 describe('type Definitions', () => {
     let astw
 
     beforeAll(async () => astw = (await prepareUnitTest('type/model.cds', locations.testOutput('type_test'))).astw)
     
-    test('All Definitions Present', async () => {
+    test('All Definitions Present', () => {
         expect(astw.tree.find(({name, nodeType}) => name === 'IntAlias' && nodeType === 'typeAliasDeclaration')).toBeTruthy()
         expect(astw.tree.find(({name, nodeType}) => name === 'Points' && nodeType === 'classDeclaration')).toBeTruthy()
         expect(astw.tree.find(({name, nodeType}) => name === 'Lines' && nodeType === 'typeAliasDeclaration')).toBeTruthy()
     })
 
-    test('Types as Properties', async () => {
+    test('Types as Properties', () => {
         const members = astw.tree.find(def => def.name === '_PersonAspect').body[0].members
         expect(members.find(({name, type}) => name === 'id' && type.full === 'IntAlias'))
         expect(members.find(({name, type}) => name === 'pos' && type.full === 'Points'))
         expect(members.find(({name, type}) => name === 'history' && type.full === 'Array' && type.args[0].full === 'Points'))
         expect(members.find(({name, type}) => name === 'line' && type.full === 'Array' && type.args[0].full === 'Points'))
+    })
+
+    test('Referring to Nested Types Uses Singular', async () => {
+        checkFunction(astw.tree.find(node => node.name === 'fn'), {
+            callCheck: ptype => check.isNullable(ptype, [st => check.isTypeReference(st, 'OuterType')])
+        })
     })
 })
