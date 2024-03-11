fa9fc92b79337862dc28281e728e24371e8ce999
Daniel O'Grady <daniel.o-grady@sap.com>
2024-03-11 17:43:20 +0100

    Properly implement columns and renaming thereof

--- test/unit/projections.test.js
+++ test/unit/projections.test.js
@@ -24,9 +24,12 @@ describe('Projection Entities', () => {
     })
 
     test('Expected Properties Present in Projection', () => {
-        expect(() => astw.exists('_FooViewAspect', 'retainMeOnce', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
-        expect(astw.exists('_FooViewAspect', 'removeMeNext', ({type}) => check.isNullable(type, [check.isNumber]))).toBeTruthy()
-        expect(astw.exists('_FooViewAspect', 'retainMeTwice', ({type}) => check.isNullable(type, [check.isNumber]))).toBeTruthy()
-        expect(astw.exists('_FooViewAspect', 'removeMe', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
+        expect(() => astw.exists('_FooViewProjectionAspect', 'removeMe', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
+        expect(() => astw.exists('_FooViewProjectionAspect', 'retainMeOnce', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
+        expect(() => astw.exists('_FooViewProjectionAspect', 'retainMeTwice', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
+        expect(() => astw.exists('_FooViewProjectionAspect', 'removeMeNext', ({type}) => check.isNullable(type, [check.isNumber]))).toThrow(Error)
+        expect(astw.exists('_FooViewProjectionAspect', 'Retained', ({type}) => check.isNullable(type, [check.isNumber]))).toBeTruthy()
+        
+        
     })
 })
