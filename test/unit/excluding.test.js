ad7dca531e3b6bd90f2ab7eaaa03ba72f9f4011b
Daniel O'Grady <103028279+daogrady@users.noreply.github.com>
2024-04-11 15:49:47 +0200

    Chore/bump-eslint (#204)
    * Add .kind property to functions/ actions
    
    * Migrate to eslint 9 and flat config
    
    * Appease linter

--- test/unit/excluding.test.js
+++ test/unit/excluding.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const { beforeAll, describe, test, expect } = require('@jest/globals')
 const { ASTWrapper } = require('../ast')
 const { locations, prepareUnitTest } = require('../util')
 
@@ -13,9 +14,9 @@ describe('Excluding Clause', () => {
         expect(new ASTWrapper(path.join(paths[1], 'index.ts')).exists('_TestObjectAspect', 'dependencies')).toBeTruthy())
 
     test('Element Gone in Projection', async () => 
-            expect(new ASTWrapper(path.join(paths[2], 'index.ts'))
-                .getAspect('_SlimTestObjectAspect')
-                .members
-                .find(({name}) => name === 'dependencies')
-            ).toBeFalsy())
+        expect(new ASTWrapper(path.join(paths[2], 'index.ts'))
+            .getAspect('_SlimTestObjectAspect')
+            .members
+            .find(({name}) => name === 'dependencies')
+        ).toBeFalsy())
 })
