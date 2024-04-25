23479ea04103b12db40c4e18119435f65c48848d
Daniel O'Grady <daniel.o-grady@sap.com>
2024-04-25 16:16:45 +0200

    Foreign keys can now also be nullable

--- test/unit/foreignkeys.test.js
+++ test/unit/foreignkeys.test.js
@@ -2,24 +2,25 @@
 
 const { beforeAll, describe, test, expect } = require('@jest/globals')
 const { locations, prepareUnitTest } = require('../util')
+const { check } = require('../ast')
 
 describe('Foreign Keys', () => {
     let astw
     beforeAll(async () => astw = (await prepareUnitTest('foreignkeys/model.cds', locations.testOutput('foreign_keys'))).astw)
 
     test('One Level Deep', async () => {
-        expect(astw.exists('_BAspect', 'c_ID', m => m.type.keyword === 'string')).toBeTruthy()
-        expect(astw.exists('_BAspect', 'd_ID', m => m.type.keyword === 'string')).toBeTruthy()
-        expect(astw.exists('_CAspect', 'e_ID', m => m.type.keyword === 'string')).toBeTruthy()
+        expect(astw.exists('_BAspect', 'c_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
+        expect(astw.exists('_BAspect', 'd_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
+        expect(astw.exists('_CAspect', 'e_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
     })
 
     test('Two Levels Deep', async () => {
-        expect(astw.exists('_AAspect', 'b_c_ID', m => m.type.keyword === 'string')).toBeTruthy()
-        expect(astw.exists('_AAspect', 'b_d_ID', m => m.type.keyword === 'string')).toBeTruthy()
-        expect(astw.exists('_BAspect', 'c_e_ID', m => m.type.keyword === 'string')).toBeTruthy()
+        expect(astw.exists('_AAspect', 'b_c_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
+        expect(astw.exists('_AAspect', 'b_d_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
+        expect(astw.exists('_BAspect', 'c_e_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
     })
 
     test('Three Levels Deep', async () => {
-        expect(astw.exists('_AAspect', 'b_c_e_ID', m => m.type.keyword === 'string')).toBeTruthy()
+        expect(astw.exists('_AAspect', 'b_c_e_ID', m => check.isNullable(m.type, [check.isString]))).toBeTruthy()
     })
 })
