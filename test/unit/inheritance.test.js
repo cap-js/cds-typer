f42615b4291a1b88b914b39144785088fe99a604
Daniel O'Grady <daniel.o-grady@sap.com>
2024-05-21 09:14:13 +0200

    Strictly derive singular from CSN in extends clause

--- test/unit/inheritance.test.js
+++ test/unit/inheritance.test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { describe, test } = require('@jest/globals')
+const { describe, test, beforeAll } = require('@jest/globals')
 const { locations, prepareUnitTest } = require('../util')
 const { checkInheritance } = require('../ast')
 const { expect } = require('@jest/globals')
@@ -8,7 +8,6 @@ const { expect } = require('@jest/globals')
 describe('Inheritance', () => {
     let ast
     beforeAll(async () => ast = (await prepareUnitTest('inheritance/model.cds', locations.testOutput('inheritance_test'))).astw.tree)
-
 
     test('Entity, Type <- Entity', async () => {
         const leaf = ast.find(n => n.name === 'LeafEntity')
@@ -27,7 +26,11 @@ describe('Inheritance', () => {
     })
 
 
-    test('Extends (-s) <- Self', async () => {
+    test('Extends Own Aspect (-s, Explicit Annotation)', async () => 
         expect(checkInheritance(ast.find(n => n.name === 'Circus'), ['_CircusAspect'])).toBe(true)
-    })
+    )
+
+    test('Extends Own Aspect (-s, No Annotation)', async () => 
+        expect(checkInheritance(ast.find(n => n.name === 'Abys'), ['_AbysAspect'])).toBe(true)
+    )
 })
