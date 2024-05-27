8fc81a160b543124ab9166df66f0c90c5668ea41
Daniel O'Grady <daniel.o-grady@sap.com>
2024-05-27 06:06:36 +0200

    Hack priority issue

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,58 +1,82 @@
+const { readdir, stat } = require('node:fs/promises')
+const { normalize } = require('node:path')
 const cds = require('@sap/cds')
-const { fs, path } = cds.utils
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
-const { readdir, stat } = require('fs/promises')
-const DEBUG = cds.debug('cli|build')
 
+const { fs, path } = cds.utils
+const DEBUG = cds.debug('cli|build')
 const BUILD_CONFIG = 'tsconfig.build.json'
+
+/**
+ * Check if a tsconfig file exists.
+ */
 const tsConfigExists = () => fs.existsSync('tsconfig.json')
+
+/**
+ * Check if separate tsconfig file that is used for building the project.
+ * @returns {boolean}
+ */
 const buildConfigExists = () => fs.existsSync(BUILD_CONFIG)
+
+/**
+ * @param {string} dir The directory to remove.
+ */
 const rmDirIfExists = dir => {
   try { fs.rmSync(dir, { recursive: true }) } catch { /* ignore */ }
 }
-const rmFiles = async (dir, exts) => Promise.all(
-  (await readdir(dir))
-    .map(async file => {
-      const filePath = path.join(dir, file)
-      if ((await stat(filePath)).isDirectory()) {
-        return rmFiles(filePath, exts)
-      } else if (exts.some(ext => file.endsWith(ext))) {
-        fs.unlinkSync(filePath)
-      }
-    })
-)
+
+/**
+ * Remove files with given extensions from a directory recursively.
+ * @param {string} dir The directory to start from.
+ * @param {string[]} exts The extensions to remove.
+ * @returns {Promise<void>}
+ */
+const rmFiles = async (dir, exts) => fs.existsSync(dir) 
+  ? Promise.all(
+    (await readdir(dir))
+      .map(async file => {
+        const filePath = path.join(dir, file)
+        if ((await stat(filePath)).isDirectory()) {
+          return rmFiles(filePath, exts)
+        } else if (exts.some(ext => file.endsWith(ext))) {
+          fs.unlinkSync(filePath)
+        }
+      })
+  )
+  : undefined
 
 // requires @sap/cds-dk version >= 7.5.0
 cds.build?.register?.('typescript', class extends cds.build.Plugin {
   static taskDefaults = { src: '.' }
   static hasTask() { return tsConfigExists() }
 
-  get priority() { return 1 } // should run after the node build task FIXME: set to 0 once that is allowed
+  // FIXME: hack to make sure dk does not complain about .priority != 1 on initial check, but runs after nodejs plugin regardless
+  // replace with just "return 0" asap
+  #priority = 1
+  get priority() { return this.#priority-- }
+
+  get #appFolder () { return cds?.env?.folders?.app ?? 'app' }
+
+  get #modelDirectoryName () {
+    try {
+      // expected format: { '#cds-models/*': [ './@cds-models/*/index.ts' ] }
+      //                                       ^^^^^^^^^^^^^^^ 
+      //                             relevant part - may be changed by user
+      const config = JSON.parse(fs.readFileSync ('tsconfig.json', 'utf8'))
+      const alias = config.compilerOptions.paths['#cds-models/*'][0]
+      const directory = alias.match(/(?:\.\/)?(.*)\/\*\/index\.ts/)[1]
+      return normalize(directory)  // could contain forward slashes in tsconfig.json
+    } catch {
+      DEBUG?.(`tsconfig.json not found, not parsable, or unconclusive. Using default model directory name`)
+    }
+    return '@cds-models'
+  }
 
   init() {
     // different from the default build output structure
     this.task.dest = path.join(cds.root, 'gen', 'srv')
-    //this.task.dest = path.join(cds.root, cds.env.build.target !== '.' ? cds.env.build.target : 'gen')
   }
-
-  get #modelDirectoryName () {
-    // FIXME: dynamically retrieve the model directory name from tsconfig.
-    // This would require us to be able to consume jsonc, as tsconfigs can contain comments.
-    return '@cds-models'
-  }
-
-  // createBuildTSConfig () {
-  //   if (fs.existsSync(BUILD_CONFIG)) return
-  //   const tsConfig = {
-  //     'extends': './tsconfig.json',
-  //     'compilerOptions': {
-  //       'outDir': 'gen'
-  //     },
-  //     'exclude': ['gen', 'app']
-  //   }
-  //   fs.writeFileSync(BUILD_CONFIG, JSON.stringify(tsConfig, null, 2))
-  // }
 
   async #runCdsTyper () {
     DEBUG?.('running cds-typer')
@@ -60,6 +84,9 @@ cds.build?.register?.('typescript', class extends cds.build.Plugin {
   }
 
   async #buildWithConfig () {
+    // FIXME: this is currently unused. We'd expect users to have a tsconfig.build.json,
+    // possibly referencing their tsconfig.json via "extends", specifying the "compilerOptions.outDir" and
+    // manually adding irrelevant folders (read: gen/ and app/) to the "exclude" array.
     DEBUG?.(`building with config ${BUILD_CONFIG}`)
     return exec(`npx tsc --project ${BUILD_CONFIG}`)
   }
@@ -70,18 +97,17 @@ cds.build?.register?.('typescript', class extends cds.build.Plugin {
     // _within_ the project directory. So we need to remove it afterwards.
     await exec(`npx tsc --outDir ${this.task.dest}`)
     rmDirIfExists(path.join(this.task.dest, 'gen'))
-    rmDirIfExists(path.join(this.task.dest, 'app'))
+    rmDirIfExists(path.join(this.task.dest, this.#appFolder))
   }
 
   async #copyCleanModel (buildDirCdsModels) {
     // copy models again, to revert transpilation thereof.
     // We only need the index.js files in un-transpiled form.
-    await this.copy('@cds-models').to(buildDirCdsModels)
+    await this.copy(this.#modelDirectoryName).to(buildDirCdsModels)
     await rmFiles(buildDirCdsModels, ['.ts'])
   }
 
   async build() {
-    //this.createBuildTSConfig()
     await this.#runCdsTyper()
     const buildDirCdsModels = path.join(this.task.dest, this.#modelDirectoryName)
     // remove the js files generated by the nodejs buildtask,
@@ -98,7 +124,6 @@ cds.build?.register?.('typescript', class extends cds.build.Plugin {
         ? new Error(error.stdout)
         : error
     }
-
     this.#copyCleanModel(buildDirCdsModels)
   }
 })
