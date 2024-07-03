02c6a37850e48ecedc95eec11d7c432d7bc0165e
Daniel O'Grady <103028279+daogrady@users.noreply.github.com>
2024-07-03 07:45:32 +0200

    Accumulate ancestors in aspects, not in exported class (#267)

--- test/unit/inheritance.test.js
+++ test/unit/inheritance.test.js
@@ -10,19 +10,25 @@ describe('Inheritance', () => {
     beforeAll(async () => ast = (await prepareUnitTest('inheritance/model.cds', locations.testOutput('inheritance_test'))).astw.tree)
 
     test('Entity, Type <- Entity', async () => {
-        const leaf = ast.find(n => n.name === 'LeafEntity')
+        const [leafAspect] = ast.find(n => n.name === '_LeafEntityAspect').body
+        const leaf = ast.find(n => n.name === 'LeafEntity')//'LeafEntity')
         // inherit from singular aspects
-        expect(checkInheritance(leaf, ['_AAspect', '_BAspect', '_TAspect', '_._ExtEAspect', '_._ExtTAspect', '_LeafEntityAspect'])).toBe(true)
+        expect(checkInheritance(leafAspect, ['_AAspect', '_BAspect', '_TAspect', '_._ExtEAspect', '_._ExtTAspect'])).toBe(true)
         // not from plural
-        expect(checkInheritance(leaf, ['_AAspects'])).toBe(false)
+        expect(checkInheritance(leafAspect, ['_AAspects'])).toBe(false)
+        // class only extends the aspect
+        expect(checkInheritance(leaf, ['_LeafEntityAspect'])).toBe(true)
     })
 
     test('Entity, Type <- Type', async () => {
+        const [leafAspect] = ast.find(n => n.name === '_LeafTypeAspect').body
         const leaf = ast.find(n => n.name === 'LeafType')
         // inherit from singular aspects
-        expect(checkInheritance(leaf, ['_AAspect', '_BAspect', '_TAspect', '_._ExtEAspect', '_._ExtTAspect', '_LeafTypeAspect'])).toBe(true)
+        expect(checkInheritance(leafAspect, ['_AAspect', '_BAspect', '_TAspect', '_._ExtEAspect', '_._ExtTAspect'])).toBe(true)
         // not from plural
-        expect(checkInheritance(leaf, ['_AAspects'])).toBe(false)
+        expect(checkInheritance(leafAspect, ['_AAspects'])).toBe(false)
+        // class only extends the aspect
+        expect(checkInheritance(leaf, ['_LeafTypeAspect'])).toBe(true)
     })
 
 
@@ -33,4 +39,11 @@ describe('Inheritance', () => {
     test('Extends Own Aspect (-s, No Annotation)', async () =>
         expect(checkInheritance(ast.find(n => n.name === 'Abys'), ['_AbysAspect'])).toBe(true)
     )
+
+    test('Multilevel Inheritance', async () => {
+        const ast = (await prepareUnitTest('inheritance/multilevel.cds', locations.testOutput('inheritance_test'))).astw.tree
+        // we can't really check the transitive inheritance relationship here, which would manifest in instances of E owning the properties from A1...
+        expect(checkInheritance(ast.find(n => n.name === '_EAspect').body[0], ['_A2Aspect'])).toBe(true)
+        expect(checkInheritance(ast.find(n => n.name === '_A2Aspect').body[0], ['_A1Aspect'])).toBe(true)
+    })
 })
