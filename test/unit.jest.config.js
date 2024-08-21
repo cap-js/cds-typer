8f64f791e89d7be42c03f316daddf20a727f8d4e
Christian Georgi <christian.georgi@sap.com>
2024-08-21 18:55:42 +0200

    Test-serve

--- test/unit.jest.config.js
+++ test/unit.jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
     displayName: 'unit',
     rootDir: '..',
     testRegex: './unit/.*.test.js',
-    globalSetup: '<rootDir>/test/unit/setup.js'
+    globalSetup: '<rootDir>/test/unit/setup.js',
+    moduleNameMapper: {
+        '#cds-models/(.*)': './_out/$1/index.js',
+    }
+
 }
