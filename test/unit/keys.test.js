979f6270ee848ba8acce08f7eeb26ab0596fd10c
Daniel O'Grady <daniel.o-grady@sap.com>
2024-09-04 11:31:11 +0200

    Adjust tests to new Key wrapper

--- test/unit/keys.test.js
+++ test/unit/keys.test.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const { check } = require('../ast')
 const { beforeAll, describe, test } = require('@jest/globals')
 const { locations, prepareUnitTest } = require('../util')
+const { expect } = require('@jest/globals')
 
 describe('KeyOf', () => {
     let astw
@@ -9,8 +11,8 @@ describe('KeyOf', () => {
     beforeAll(async () => astw = (await prepareUnitTest('keys/model.cds', locations.testOutput('keys_test'))).astw)
 
     test('test', () => {
-        const actions = astw.getAspectProperty('_CAspect', 'keys')
-        console.log(actions)
+        const c_ID = astw.getAspectProperty('_CAspect', 'c')
+        expect(check.isKeyOf(c_ID.type, check.isString)).toBe(true)
     })
 
 })
