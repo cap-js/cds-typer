d29aed31c67e086eb92661b0b9ec0d6c228b95cd
Daniel O'Grady <103028279+daogrady@users.noreply.github.com>
2024-10-17 09:16:26 +0200

    Safely override `.kind` and `Buffer` improvements (#367)

--- test/unit/files/actions/model.ts
+++ test/unit/files/actions/model.ts
@@ -38,15 +38,15 @@ export class S extends cds.ApplicationService { async init(){
 
   this.on(getOneExternalType,        req => { return {extType:1} satisfies ExternalType})
   this.on(getManyExternalTypes,      req => { return [{extType:1}] satisfies ExternalType[] })
-  this.on(fSingleParamSingleReturn,  req => { const {val} = req.data; return {e1:val.e1} satisfies E })
-  this.on(fSingleParamManyReturn,    req => { const {val} = req.data; return [{e1:val.e1}] satisfies E[] })
-  this.on(fManyParamManyReturn,      req => { const {val} = req.data; return val satisfies E[] })
-  this.on(fManyParamSingleReturn,    req => { const {val} = req.data; return {e1:val[0].e1} satisfies E })
+  this.on(fSingleParamSingleReturn,  req => { const {val} = req.data; return {e1:val?.e1} satisfies E })
+  this.on(fSingleParamManyReturn,    req => { const {val} = req.data; return [{e1:val?.e1}] satisfies E[] })
+  this.on(fManyParamManyReturn,      req => { const {val} = req.data; return val! satisfies E[] })
+  this.on(fManyParamSingleReturn,    req => { const {val} = req.data; return {e1:val?.at(0)?.e1} satisfies E })
 
-  this.on(aSingleParamSingleReturn,  req => { const {val} = req.data; return {e1:val.e1} satisfies E })
-  this.on(aSingleParamManyReturn,    req => { const {val} = req.data; return [{e1:val.e1}] satisfies E[] })
-  this.on(aManyParamManyReturn,      req => { const {val} = req.data; return val satisfies E[] })
-  this.on(aManyParamSingleReturn,    req => { const {val} = req.data; return {e1:val[0].e1} satisfies E })
+  this.on(aSingleParamSingleReturn,  req => { const {val} = req.data; return {e1:val?.e1} satisfies E })
+  this.on(aSingleParamManyReturn,    req => { const {val} = req.data; return [{e1:val?.e1}] satisfies E[] })
+  this.on(aManyParamManyReturn,      req => { const {val} = req.data; return val! satisfies E[] })
+  this.on(aManyParamSingleReturn,    req => { const {val} = req.data; return {e1:val?.at(0)?.e1} satisfies E })
 
   this.on(aMandatoryParam,  req => {
     false satisfies IsKeyOptional<typeof req.data, 'val1'>
@@ -59,27 +59,27 @@ export class S extends cds.ApplicationService { async init(){
   this.on(free2,      req => { return {extType:1} satisfies ExternalType })
   this.on(free3,      req => { return {extRoot:1} satisfies ExternalInRoot})
   this.on(freevoid,   req => { return undefined satisfies void })
-  this.on(freetypeof, req => { req.data.p satisfies number })
+  this.on(freetypeof, req => { req.data.p! satisfies number })
   this.on(free4,      req => { return { extType2:1 } satisfies ExternalType2 })
 
   // calling actions
-  const s2  = await cds.connect.to(S2)
-  await s2.a1({p1: '', p2: [ { extType2: 1 } ]}) satisfies ExternalType
-  await s2.a1('', [ { extType2: 1 } ]) satisfies ExternalType
-  await s2.a2({p1: '', p3: 1}) satisfies ExternalType
-  await s2.a2('', [], 1) satisfies ExternalType
+  const s2:S2  = await cds.connect.to(S2)
+  await s2.a1({p1: '', p2: [ { extType2: 1 } ]}) satisfies ExternalType | null
+  await s2.a1('', [ { extType2: 1 } ]) satisfies ExternalType | null
+  await s2.a2({p1: '', p3: 1}) satisfies ExternalType | null
+  await s2.a2('', [], 1) satisfies ExternalType | null
 
-  await s_.aRfcStyle({INPUT: ''}) satisfies ExternalType
+  await s_.aRfcStyle({INPUT: ''}) satisfies ExternalType | null
   //@ts-expect-error
   await s_.aRfcStyle('') // no positional call style allowed for RFC actions
 
   // docs -> hover over actions
   // provider side
-  this.on(aDocOneLine,    req => { const {val1, val2} = req.data; return {e1:val1[0].e1} satisfies E })
+  this.on(aDocOneLine,    req => { const {val1, val2} = req.data; return {e1:val1?.e1} satisfies E  | null})
   // caller side
   s_.aDocOneLine({val1: {}, val2: {}})
   // provider side
-  this.on(aDocMoreLinesWithBadChar, req => { const {val} = req.data; return {e1:val[0].e1} satisfies E })
+  this.on(aDocMoreLinesWithBadChar, req => { const {val} = req.data; return {e1:val?.e1} satisfies E | null})
   // caller side
   s_.aDocMoreLinesWithBadChar({val: {}})
 
--- test/unit/files/keys/model.ts
+++ test/unit/files/keys/model.ts
@@ -8,6 +8,6 @@ C.keys.e2 // inherited from E2
 C.keys.c  // own key
 // @ts-expect-error - own non-key property not contained in .keys
 C.keys.d 
-new C().c.toLowerCase()  // still a regular string
+new C().c?.toLowerCase()  // still a regular string
 // @ts-expect-error - Symbol marker not accessible
 new C().c.key
