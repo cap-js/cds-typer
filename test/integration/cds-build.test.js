4724140fb41398604de55be5b1ef45e0bac7586d
Daniel O'Grady <daniel.o-grady@sap.com>
2024-11-25 13:44:15 +0100

    Make test more robust

--- test/integration/cds-build.test.js
+++ test/integration/cds-build.test.js
@@ -11,21 +11,32 @@ const { log } = require('@sap/cds')
 
 describe('cds build', () => {
    test('Dummy Project', async () => {
-    const dir = path.join(tmpdir(), 'dummy-project')
+    const dir = path.join(__dirname, '_out', 'dummy-project')  // local dir?
     const typer = path.join(__dirname, '..', '..')
     try { fs.unlinkSync(dir) } catch {}  // could be first run and not exist
-    try { fs.mkdirSync(dir) } catch {}  // could already exist from former run
+    try { fs.mkdirSync(dir, { recursive: true }) } catch {}  // could already exist from former run
     [
         'cds init',
         'cds add tiny-sample',
         'npm i @sap/cds-dk',
-        'npm i',
-        `npm i file:${typer}`
+        //'npm i typescript',
+        //'npm i',
+        //`npm i --save-dev file:${typer}`
     ].forEach(cmd => {
         try {
             execSync(cmd, { cwd: dir }) // could have been initialised before
         } catch {}
     });
-    ['cds-typer "*" --outputDirectory @cds-models', 'cds build'].forEach(cmd => execSync(cmd, { cwd: dir }))
+    const packageJsonPath = path.join(dir, 'package.json')
+    const packageJson = require(packageJsonPath)
+    delete packageJson.dependencies['@sap/cds']
+    packageJson.devDependencies['typescript'] = '*'
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    [
+        'npm i @sap/cds-dk',
+        'rm -rf node_modules/@sap/cds',
+        'cds-typer "*" --outputDirectory @cds-models',
+        'cds build'
+    ].forEach(cmd => execSync(cmd, { cwd: dir }))
    })
 })
