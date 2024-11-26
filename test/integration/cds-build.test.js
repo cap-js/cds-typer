d155fc4f323a4db73daf29eddb05949521175c52
Daniel O'Grady <daniel.o-grady@sap.com>
2024-11-26 08:19:15 +0100

    Condense test

--- test/integration/cds-build.test.js
+++ test/integration/cds-build.test.js
@@ -1,42 +1,36 @@
 'use strict'
 
-const { tmpdir } = require('os')
-const fs = require('fs')
-const path = require('path')
-const cds2ts = require('../../lib/compile')
-const { resolveAliases, locations, createProject } = require('../util')
-const { execSync } = require('child_process')
-const { fail } = require('assert')
-const { log } = require('@sap/cds')
+const fs = require('node:fs')
+const path = require('node:path')
+const { execSync } = require('node:child_process')
 
-describe('cds build', () => {
-   test('Dummy Project', async () => {
-    const dir = path.join(__dirname, '_out', 'dummy-project')  // local dir?
-    const typer = path.join(__dirname, '..', '..')
+const dir = path.join(__dirname, '_out', 'dummy-project')
+const typer = path.join(__dirname, '..', '..')
+
+beforeAll(()=>{
     try { fs.unlinkSync(dir) } catch {}  // could be first run and not exist
     try { fs.mkdirSync(dir, { recursive: true }) } catch {}  // could already exist from former run
-    [
+    // copy .tar over. This ensures we end up with a cds-typer as if it was downloaded from npmjs,
+    // without its node_modules. That is import to avoid having multiple instances of sap/cds(-dk)
+    // which will cause confusion during build-plugin-registration.
+    execSync(`npm pack; mv ${path.join(typer, '*cds-typer-*.tgz')} ${path.join(dir, 'typer.tgz')}`, { cwd: typer })
+    execSync([
         'cds init',
-        'cds add tiny-sample',
-        'npm i @sap/cds-dk',
-        //'npm i typescript',
-        //'npm i',
-        //`npm i --save-dev file:${typer}`
-    ].forEach(cmd => {
-        try {
-            execSync(cmd, { cwd: dir }) // could have been initialised before
-        } catch {}
-    });
+        'npx tsc --init',
+        'cds add sample',
+        'npm i @sap/cds-dk typer.tgz',
+        'cds-typer "*" --outputDirectory @cds-models'
+    ].join(';'), { cwd: dir })
+    // clean package.json
     const packageJsonPath = path.join(dir, 'package.json')
     const packageJson = require(packageJsonPath)
     delete packageJson.dependencies['@sap/cds']
     packageJson.devDependencies['typescript'] = '*'
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-    [
-        'npm i @sap/cds-dk',
-        'rm -rf node_modules/@sap/cds',
-        'cds-typer "*" --outputDirectory @cds-models',
-        'cds build'
-    ].forEach(cmd => execSync(cmd, { cwd: dir }))
-   })
+})
+
+describe('cds build', () => {
+    test('Dummy Project', async () => {
+        execSync('cds build', { cwd: dir })
+    })
 })
